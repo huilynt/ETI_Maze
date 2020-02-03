ab04e5aec84aca6db730a98e2182c45f0ccbce32
tayboonkai <56620426+tayboonkai@users.noreply.github.com>
2020-02-04 01:22:57 +0800

    Updated test cases
    update for test cases on configuring maze feature: added test details

---
+++ src/maze_functions.py
@@ -0,0 +1,16 @@
+# functions for the program
+config_menu= ["Create Wall", "Create Passageway", "Create Start Point", "Create End Point"]
+
+# Display main menu
+
+# [1] Read and load maze from file
+
+# [2] View maze
+
+# [3] Play maze game
+
+# [4] Configure current maze
+def configure_maze():
+	print("CONFIGURATION MENU")
+	print("==================")
+	
--- test_configuremaze.py
+++ test_configuremaze.py
@@ -1,21 +1,37 @@
 import pytest
+#From src import main_functions
 
 #Test for displaying configuration menu
-
+def test_display_config_menu():
+	working= displayConfigMenu()
+	assert working = " Configuration Menu is displayed successfully"
 
 #Test for creating wall
-
+def test_create_wall():
+	createWall(2,1)
+	#assert maze[2][1] == "X"
 
 #Test for creating passageway
-
+def test_create_passageway():
+	createPassageway(3,1)
+	#assert maze[3][1] == "O"
 
 #Test for creating startpoint
-
+def test_create_startpoint():
+	createStartpoint(5,1)
+	#assert maze[5][1] == "A"
 
 #Test for creating endpoint
-
+def test_create_endpoint():
+	createEndpoint(1,8)
+	#assert maze[1][8] == "B"
 
 #Test for exiting to main menu from configuration
-
+def test_exit_mainmenu():
+	choice= configChoice("B")
+	#assert value == True
 
 #Test for exiting to configuration menu
+def test_exit_configmenu():
+	choice= configChoice("M")
+	#assert value == True

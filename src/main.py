ed0c40759fcf2714918aa2003a3e1f5674186c4f
tayboonkai <56620426+tayboonkai@users.noreply.github.com>
2020-02-08 21:39:11 +0800

    Integration of master branch code
    Integration of master branch code

--- src/main.py
+++ src/main.py
@@ -1,13 +1,38 @@
-# program will run here
 from maze_functions import *
 import csv
 
-while True:
-    maze=read_file()
-    display_maze(maze)
+# Main program structure
+# running keeps the program alive, break my option_functions returning False
+# more info in option_functions() in maze_functions.py
+running = True
+maze = []
+start = []
+end = []
+while running:
     display_menu()
-    option= get_option()
-    option_function(option,maze)
+    option = get_option()
+    if option == 1:
+        print('[1] Read and load maze from file')
+        maze = read_file()
+        start, end = store_location(maze)
+    elif option == 2:
+        print('[2] View maze')
+        display_maze(maze)
+    elif option == 3:
+        print('[3] Play maze game')
+    elif option == 4:
+        print('[4] Configure current maze')
+        print("")
+        display_maze(maze)
+        print("")
+        option_function(option,maze)
+    elif option == 0:
+        print('[0] Exit maze')
+        running = False
+    else:
+        print('Invalid option! Please try again!')
+
+
 
 
 
--- src/maze_functions.py
+++ src/maze_functions.py
@@ -1,31 +1,65 @@
 # functions for the program
-config_menu= ["Create Wall", "Create Passageway", "Create Start Point", "Create End Point"]
+
 import csv
 
+
 # Display main menu
+# Display main menu and return it
 def display_menu():
-	menu = ('''
+    menu = ('''
+    MAIN MENU
+    =========
+    [1] Read and load maze from file
+    [2] View maze
+    [3] Play maze game
+    [4] Configure current maze
 
-	MAIN MENU
+    [0] Exit maze
+    ''')
+    print(menu)
+    return menu
 
-	=========
 
-	[4] Configure current maze
-	''')
-	print(menu)
-	return(menu)
-
+# Get user input for option
+# validate (option is in 0 to 4 range) and return it as int
+# else return -1 as invalid indicator
 def get_option():
 	option = input("Enter your option:")
-	if (0 <= int(option) < 5):
+	if (option.isdigit() and 0 <= int(option) < 5):
 		option = int(option)
 	else:
-		print("Invalid Option!")
+		option = -1
 	return option
+
+
+# # Given a valid option (from get_option())
+# # print the option title
+# # run the corresponding function for each option
+# # breaks the program by returning a False when option is 0
+# # else always return True to keep main program running
+# # def option_functions(option):
+#     maze = []
+#     return_var = True
+#     if option == 1:
+#         print('Read and load maze from file')
+#         maze = read_file()
+#     elif option == 2:
+#         print('View maze')
+
+#     elif option == 3:
+#         print('Play maze game')
+#     elif option == 4:
+#         print('Configure current maze')
+#     elif option == 0:
+#         print('Exit maze')
+#         return_var = False
+#     else:
+#         print('Invalid option! Please try again!')
+
+#     return return_var, maze
 
 def option_function(option,maze):
 	if option == 4:
-		print('Configure current maze')
 		Display_config_menu()
 		config_options(maze)
 	else:
@@ -33,9 +67,7 @@ def option_function(option,maze):
 
 
 # [1] Read and load maze from file
-#Code from read file feature done by HuiLin
 def read_file():
-    print('Option [1] Read and load maze from file')
     file_name = input('Enter the name of the data file: ')
     maze = []
     num_lines = 0
@@ -52,17 +84,36 @@ def read_file():
     return maze
 
 
+def store_location(maze):
+    start_coordinate = []
+    end_coordinate = []
+    for row_index, row in enumerate(maze):
+        for column_index, column in enumerate(row):
+            if (column == 'A'):
+                start_coordinate.append(column_index + 1)
+                start_coordinate.append(row_index + 1)
+                break
+            elif (column == 'B'):
+                end_coordinate.append(column_index + 1)
+                end_coordinate.append(row_index + 1)
+                break
+    if (len(start_coordinate) == 0 or len(end_coordinate) == 0):
+        print('Invalid maze!')
+        return -1, -1
+
+    return start_coordinate, end_coordinate
+
 # [2] View maze
-#Code from display maze feature done by HuiLin
 def display_maze(maze):
     disp_maze = '\n'.join(str(row) for row in maze)
-    print(disp_maze, end='')
+    print(disp_maze)
     return disp_maze
 
 # [3] Play maze game
 
 # [4] Configure current maze
 #Display configuration menu with options
+config_menu= ["Create Wall", "Create Passageway", "Create Start Point", "Create End Point"]
 def Display_config_menu():
 	
 	print("CONFIGURATION MENU")

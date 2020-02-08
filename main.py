8f48e17bf01b0433f78c2243402c9e4921a5f922
liewlucas <39293732+liewlucas@users.noreply.github.com>
2020-02-08 16:27:04 +0800

    fix

--- main.py
+++
@@ -1,7 +0,0 @@
-# program will run here
-from maze_functions import *
-
-# Main program structure
-# running keeps the program alive, break my option_functions returning False
-# more info in option_functions() in maze_functions.py
-startgame()
